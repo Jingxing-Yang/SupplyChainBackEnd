8e37f0d07940dccfb0eae44f79af513e99ce56d0
Jingxing Yang <jingxiny@usc.edu>
2016-11-19 12:01:54 -0800

    Uploaded Container class file

---
+++ Container.py
@@ -0,0 +1,22 @@
+"""THe class for container. It store all the information for a container"""
+class Container:
+	def _init_(self,vesselName,shippingLine,containerID,billOfLanding,size,loadPort,unloadPort,
+		isLocal,inlandPoint,arrivingTerminal,arrivalTime,cutOffTime,departTime,availTime,entryMode,existMode)
+		self.vesselName = vesselName
+		self.shippingLine = shippingLine
+		self.containerID = containerID
+		self.billOfLanding = billOfLanding
+		self.size = size
+		self.loadPort = loadPort
+		self.unloadPort = unloadPort
+		self.isLocal = isLocal
+		self.arrivingTerminal = arrivingTerminal
+		self.inlandPoint = inlandPoint
+		self.arrivalTime = arrivalTime
+		self.cutOffTime = cutOffTime
+		self.departTime = departTime
+		self.availTime = availTime
+		self.entryMode = entryMode
+		self.existMode = existMode
+
+
