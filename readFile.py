19160b8f6de187065b9fa662a0e9927379171702
Eric Yihan Chen <ericyihc@usc.edu>
2016-11-19 18:56:41 -0800

    Update classes

--- readFile.py
+++ readFile.py
@@ -1,6 +1,4 @@
-
 import openpyxl as pyxl
-
 
 class Container:
     def __init__(self, vesselName, shippingLine, containerID, billOfLanding, size,
@@ -63,7 +61,6 @@ class Vessel:
     def upload(self, current_container):
         self.containers.append(current_container)
         current_container.location = "vessel"
-        
 
     def unload(self, warehouse):
         for container in self.containers:
@@ -72,64 +69,82 @@ class Vessel:
     def show(self):
         print("Vessel #{} size: {}/{}".format(self.name, len(self.containers), self.size))
 
+class Port:
+    def __init__(self, name, vessel, warehouse):
+        self.name = "Los Angeles"
+        self.vessel = vessel
+        self.warehouse = warehouse
+        self.car_capacity = 100
+        self.current_capacity = 0
+        self.estimated_capacity = 0
+
+    def load_operation_time(self):
+        self.estimated_depart_time.append()
+        self.estimated_arrival_time.append()
+        self.estimated_curoff_time.append()
+
+    def unload(self, estimated_depart_time):
+        for operation in estimated_depart_time:
+            if time == operation:
+                # add stuff here
+                pass
 
 """Pass in the path and file name
 Return a list of vessels """
 def readFile(fileName):
-	wb = pyxl.load_workbook(fileName)
-	sheet = wb['Sheet1']
-	vesselList = []
-	for row in sheet.iter_rows(row_offset=1):
-		name = row[1].value
-		if name == None:
-			break;
-		else: 
-			outBound = (row[2]=="Outbound")
-			cargoType = row[3].value
-			shipppingLine = row[5].value
-			containerID = row[6].value
-			billOfLanding = row[7].value
-			containerSize = row[8].value
-			loadPort = row[9].value
-			unloadPort = row[10].value
-			isLocal = (row[11].value=="Local")
-			inlandPoint = row[12].value
-			voyageNum = row[13].value
-			arrivingTerminal = row[14].value
-			ariivingTime = row[15].value
-			cutOffTime = row[16].value
-			departTime = row[17].value
-			availTime = row[18].value
-			entryMode = row[19].value
-			exitMode = row[20].value
+    wb = pyxl.load_workbook(fileName)
+    sheet = wb['Sheet1']
+    vesselList = []
+    for row in sheet.iter_rows(row_offset=1):
+        name = row[1].value
+        if name == None:
+            break;
+        else: 
+            outBound = (row[2]=="Outbound")
+            cargoType = row[3].value
+            shipppingLine = row[5].value
+            containerID = row[6].value
+            billOfLanding = row[7].value
+            containerSize = row[8].value
+            loadPort = row[9].value
+            unloadPort = row[10].value
+            isLocal = (row[11].value=="Local")
+            inlandPoint = row[12].value
+            voyageNum = row[13].value
+            arrivingTerminal = row[14].value
+            ariivingTime = row[15].value
+            cutOffTime = row[16].value
+            departTime = row[17].value
+            availTime = row[18].value
+            entryMode = row[19].value
+            exitMode = row[20].value
 
-			newContainer = Container(name,shipppingLine,containerID,billOfLanding,containerSize,loadPort,unloadPort,isLocal,inlandPoint,arrivingTerminal,availTime,entryMode,exitMode)
-			""" when list is empty, simply push a new vessel"""
-			if len(vesselList) == 0:
-				newVessel = Vessel(name,outBound,isLocal,voyageNum,ariivingTime,cutOffTime,departTime)
-				newVessel.upload(newContainer)
-				vesselList.append(newVessel)
-			else:
-				exist = False
-				for myVessel in vesselList:
-					if myVessel.name == name:
-						myVessel.upload(newContainer)
-						exist = True
-						break
-				if 	exist==False:
-					newVessel = Vessel(name,outBound,isLocal,voyageNum,
-										ariivingTime,cutOffTime,departTime)
-					newVessel.upload(newContainer)
-					vesselList.append(newVessel)
-	return vesselList
+            newContainer = Container(name,shipppingLine,containerID,billOfLanding,containerSize,loadPort,unloadPort,isLocal,inlandPoint,arrivingTerminal,availTime,entryMode,exitMode)
+            """ when list is empty, simply push a new vessel"""
+            if len(vesselList) == 0:
+                newVessel = Vessel(name,outBound,isLocal,voyageNum,ariivingTime,cutOffTime,departTime)
+                newVessel.upload(newContainer)
+                vesselList.append(newVessel)
+            else:
+                exist = False
+                for myVessel in vesselList:
+                    if myVessel.name == name:
+                        myVessel.upload(newContainer)
+                        exist = True
+                        break
+                if  exist==False:
+                    newVessel = Vessel(name,outBound,isLocal,voyageNum,
+                    ariivingTime,cutOffTime,departTime)
+                    newVessel.upload(newContainer)
+                    vesselList.append(newVessel)
+    return vesselList
 
-""" Test code
+
 fileName = "resources/test.xlsx"
 myList = readFile(fileName)
 for myVessel in myList:
-	print myVessel.name
-	for myContainer in myVessel.containers:
-		print myContainer.containerID	
+    print(myVessel.name)
+    for myContainer in myVessel.containers:
+        print(myContainer.containerID)
 
-"""
-	
+    
